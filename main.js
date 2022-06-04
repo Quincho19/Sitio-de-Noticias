0fbec61f706c669b51e9c6b947a97350c084aaca
Quincho19 <halanween.32@gmail.com>
2022-06-04 17:49:44 -0600

    Final

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".boton").addEventListener("click", () =>{
+    document.querySelector(".nav-menu").classList.toggle("show")
+}); 
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.New-cards', {delay: 500});
+ScrollReveal().reveal('.bannerUnos', {delay: 500});
+ScrollReveal().reveal('.bannerDos', {delay: 500});
